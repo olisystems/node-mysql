8ac9ea2c716811ee80f66c2683dfc46de1e48e8f
m-yahya <yahya_gis@hotmail.com>
2019-05-26 19:20:50 +0200

    Remove log value

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
     $('#allUsersButton').click(() => {
-        console.log('hello')
         $.ajax({
             url: 'http://localhost:3000/results',
             type: 'GET',
