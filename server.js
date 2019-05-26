69a54f1a230cb06b499412b7183bb3f2e9205db5
m-yahya <yahya_gis@hotmail.com>
2019-05-26 19:02:34 +0200

    Add routes

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const routes = require("./routes/routes.js")
 const app = express()
 
 // use body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('cHello World!'))
+// add routes
+app.use(express.static('app'));
+routes(app);
 
 const PORT = 3000;
 app.listen(PORT, () => {
